b9531351748e1f4765c0d16d582b265002088754
Jack Yaffe <44579282+jyaffe@users.noreply.github.com>
2020-09-09 13:14:33 +0100

    added utility functions to init file )

--- sdk/lusid_drive/utilities/__init__.py
+++ sdk/lusid_drive/utilities/__init__.py
@@ -5,3 +5,5 @@ from lusid_drive.utilities.api_client_factory import ApiClientFactory
 from lusid_drive.utilities.lusid_drive_retry import lusid_drive_retry
 from lusid_drive.utilities.proxy_config import ProxyConfig
 from lusid_drive.utilities.api_configuration import ApiConfiguration
+from lusid_drive.utilities.utility_functions import get_file_id
+from lusid_drive.utilities.utility_functions import get_folder_id
